013558feb8847c35fc2c1dbce82602a9c0130da5
DESKTOP-11EHA3C\maked <makedohska@mail.ru>
2017-03-14 10:49:02 +0200

    kek

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    class Program
+    {
+        {
+            private A()
+            {
+                Console.WriteLine("a");
+            }
+            class B : A
+            {
+                public B()
+                {
+                    Console.WriteLine("B");
+                }
+            }
+            static void Main(string[] args)
+            {
+                A a = new B();
+            }
+        }
+    }
+}
